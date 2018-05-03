4d32477f3129d86fd87432b5127b79637aa00aec
анна <@DESKTOP-14IJIEU>
2018-05-03 11:09:38 +0500

    Добавьте файлы проекта.

---
+++ School Schedule/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(School_Schedule.Startup))]
+namespace School_Schedule
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
